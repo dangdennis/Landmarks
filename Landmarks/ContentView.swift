bcb0c3b86b414e83bf6252763ca64fec49e32dc5
Dennis Dang <dangggdennis@gmail.com>
2019-06-22 23:56:46 -0400

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Dennis Dang on 6/22/19.
+//  Copyright © 2019 Dennis Dang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
